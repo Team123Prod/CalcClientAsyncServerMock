850b1672131cd19c5a78111e0570c2d2808ecea4
Novak-Oksana <oksana.babaeva@gmail.com>
2017-11-17 00:36:30 +0200

    Add project files.

---
+++ CalcClientAsync/Client.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClientAsync
+{
+    public class Client
+    {
+        private HttpClient client;
+        private string uri;
+
+        public Client(string servAddress)
+        {
+            client = new HttpClient();
+            uri = servAddress;
+        }
+
+        public async Task<int> RequestCalcResult(int a, int b, char op)
+        {
+            var temp = uri + "?a=" + a + "&b=" + b + "&op=" + op;
+            string response = await client.GetStringAsync(temp);
+            return Convert.ToInt32(response);
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using CalcClientAsync;
+using System.Threading.Tasks;
+using HttpMock;
+
+namespace Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        Client client = null;
+        IHttpServer serverMock;
+        string uri = "http://localhost:1234";
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            serverMock = HttpMockRepository.At(uri);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new Client(uri);
+        }
+
+        [Test]
+        [TestCase(1, 5, '+', "6")]
+        [TestCase(10, 2, '-', "8")]
+        [TestCase(12, 2, '*', "24")]
+        [TestCase(15, 3, '/', "5")]
+        public void Test_RequestCalcResult(int x, int y, char op, string res)
+        {
+            serverMock.Stub(z => z.Get("/*"))
+                    .Return(res)
+                    .OK();
+
+            Task.Run(() =>
+            {
+                int result = Task.Run(() => client.RequestCalcResult(x, y, op)).Result;
+                return result;
+            }).ContinueWith((e) => { Assert.AreEqual(res, e); });           
+        }
+    }
+}
